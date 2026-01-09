f4f778fd86c7131bbc116d15a32dec1b65ea2680
Mysterybox <rokas.rabikauskas@gmail.com>
2026-01-09 19:12:10 +0200

    updated

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 from pygame import *
+
+# Classes
+class GameSprite(sprite.Sprite):
+    def __init__(self,sprite_image,sprite_x,sprite_y,sprite_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(sprite_image),object_size)
+        self.speed = sprite_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = sprite_x
+        self.rect.y = sprite_y
+    def reset(self):
+        win.blit(self.image,(self.rect.x,self.rect.y))
